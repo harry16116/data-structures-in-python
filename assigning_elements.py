31bc952c821672f00af1f1cfa59b88591619c8f7
harry16116 <63609521+harry16116@users.noreply.github.com>
2020-05-25 01:45:46 +0530

    Create assigning_elements.py

---
+++ assigning_elements.py
@@ -0,0 +1,10 @@
+list=[1,2,3,4]#creating a list
+print(list)#printing a list
+print(list[1])#for accesing single element
+tup = (1, 2, 3, 4, 5, 6, 7 )#creating a tuple
+print(tup[:])#for printing all elements in tuples
+dict = {"harry" : 19, "karthik" : 20, "aish" : 20, "random" : 10}#creating dic
+print (dict)#before deleting
+del dict['random']
+print(dict)#after deleting
+
